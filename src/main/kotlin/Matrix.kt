07d12904fcf11bb7f27c56a70715a07c2bc4386d
Viktor <talapa.viktor@gmail.com>
2019-11-14 16:54:46 +0100

    Development

--- src/main/kotlin/Matrix.kt
+++ src/main/kotlin/Matrix.kt
@@ -6,32 +6,35 @@ open class Matrix {
     protected val m: Int
     protected val n: Int
 
-    @Throws(IllegalArgumentException::class)
+    constructor(values: Array<DoubleArray>) {
+        data = values
+        m = values.size
+        n = values[0].size
+    }
+
     constructor(rows: Int, columns: Int, value: Double = 0.0) {
         require(rows >= 1) { "Row parameter must be at least 1." }
         require(columns >= 1) { "Column parameter must be at least 1." }
         m = rows
         n = columns
-        data = Array(m) { DoubleArray(n) }
-        for (i in 0 until m)
-            for (j in 0 until n)
-                data[i][j] = value
-    }
-
-    constructor(values: Array<DoubleArray>) : this(values.size, values[0].size) {
-        for (i in 0 until m)
-            for (j in 0 until n)
-                data[i][j] = values[i][j]
+        data = Array(m) { DoubleArray(n) { value } }
     }
 
     constructor(A: Matrix) : this(A.data)
 
     /*
-    @Throws(IllegalArgumentException::class)
     constructor(values: DoubleArray, rows: Int) {
         TODO
     }
     */
+
+    operator fun get(i: Int, j: Int): Double {
+        return data[i][j]
+    }
+
+    operator fun set(i: Int, j: Int, x: Double) {
+        data[i][j] = x
+    }
 
     fun getColumn(columnIndex: Int): DoubleArray {
         val column = DoubleArray(m)
@@ -40,7 +43,7 @@ open class Matrix {
         return column
     }
 
-    fun getRow(rowIndex: Int) : DoubleArray {
+    fun getRow(rowIndex: Int): DoubleArray {
         return data[rowIndex]
     }
 
@@ -66,20 +69,41 @@ open class Matrix {
         return A
     }
 
+    override fun toString(): String {
+        val result = StringBuilder()
+        for (row in data)
+            for (i in 0 until n) {
+                result.append((row[i]))
+                if (i < n - 1)
+                    result.append(',')
+                else
+                    result.append(';')
+            }
+        return result.toString()
+    }
+
     companion object {
 
-        fun identity(order: Int): SquareMatrix {
-            val I = SquareMatrix(order)
+        fun identity(order: Int): Matrix {
+            val I = Matrix(order, order)
             for (i in 0 until order)
                 I.data[i][i] = 1.0
             return I
         }
 
-        fun generate(rows: Int, columns: Int, minValue: Double = 0.0, maxValue: Double = 1.0): Matrix {
+        fun generate(rows: Int, columns: Int, minValue: Double, maxValue: Double): Matrix {
             val A = Matrix(rows, columns)
             for (i in 0 until A.m)
                 for (j in 0 until A.n)
-                    A.data[i][j] = Random.nextDouble(minValue, maxValue)
+                    A[i, j] = Random.nextDouble(minValue, maxValue)
+            return A
+        }
+
+        fun generate(rows: Int, columns: Int, minValue: Int = 0, maxValue: Int = 9): Matrix {
+            val A = Matrix(rows, columns)
+            for (i in 0 until A.m)
+                for (j in 0 until A.n)
+                    A[i, j] = Random.nextInt(minValue, maxValue).toDouble()
             return A
         }
     }
--- src/main/kotlin/SquareMatrix.kt
+++
@@ -1,5 +0,0 @@
-class SquareMatrix : Matrix {
-
-    @Throws(IllegalArgumentException::class)
-    constructor(order: Int, value: Double = 0.0) : super(order, order, value)
-}
